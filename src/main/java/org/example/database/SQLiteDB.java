1c8aa47cba79329e92535061bfee52297713a954
Iliailineya <ilavituk011@gmail.com>
2023-10-04 15:23:41 +0300

    Fixed bug with database connection commit

--- src/main/java/org/example/database/SQLiteDB.java
+++ src/main/java/org/example/database/SQLiteDB.java
@@ -9,20 +9,18 @@ import java.util.logging.Logger;
 import static org.example.util.Constants.DATABASE_URL;
 
 public class SQLiteDB {
-
     private static final Logger logger = Logger.getLogger(SQLiteDB.class.getName());
-
     private static Connection connection;
-
     public static Connection getConnection() {
-        if (connection == null) {
-            try {
+        try {
+            if (connection == null || connection.isClosed()) {
+                // Если соединение еще не создано или закрыто, создаем новое соединение.
                 // Указываем путь к файлу базы данных SQLite (users.db)
                 connection = DriverManager.getConnection(DATABASE_URL);
-            } catch (SQLException e) {
-                logger.warning(e.getMessage() + Arrays.toString(e.getStackTrace()));
-                e.printStackTrace();
             }
+        } catch (SQLException e) {
+            logger.warning(e.getMessage() + Arrays.toString(e.getStackTrace()));
+            e.printStackTrace();
         }
         return connection;
     }
